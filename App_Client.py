b6588b3ae8e4af7550bd8e819c5bd3ce0ef40c5c
Shoh Sewell <shohsewell@gmail.com>
2023-04-14 12:57:04 -0700

    Client sending message to server functionality
    The client can now send a message to the server and the server can receive the message and display it to the terminal.

--- App_Client.py
+++ App_Client.py
@@ -4,8 +4,12 @@ import threading
 PORT = 1234
 SERVER = "localhost"
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('localhost', 1234))
 
 msg = s.recv(1024)   #Decide how much data we want to receive at a time (buffer)
 print(msg.decode("utf-8"))   #sockets communicating in bytes
+
+while True:
+	inputBuffer = str(input())
+	s.send(inputBuffer.encode("utf-8"))
--- App_Server.py
+++ App_Server.py
@@ -7,7 +7,7 @@ SERVER = "localhost"
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((SERVER, PORT))
-s.listen(5)   #incase of multiple connections, queue of 5
+s.listen()   #incase of multiple connections, queue of 5
 
 def start_server():
     print('Connection has been established on {}:{}'.format(SERVER, PORT))
@@ -15,15 +15,21 @@ def start_server():
     #listen for conenctions
     while True:
         clientsocket, address= s.accept()      #if anyone connects, accept
+        
         print(f"Connection from {address} has been established!")
-        clientsocket.send(bytes("Welcome to the chat room", "utf-8"))
+        clientsocket.send("Welcome to the chat room".encode( "utf-8"))
+        
+        break
+        
+        
 
         #new thread creation for client connections
         #c_thread = threading.Thread(target= handle_client, args=(c_socket, c_address))
         #c_thread.start()
+        
+    while True:
+        msg = clientsocket.recv(1024)
+        print(msg.decode("utf-8"))
 
 #start the server
 start_server()
-
-#test github push
-#test githib push2
