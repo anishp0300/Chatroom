0f2b73b2f9a0083b667e69627e97e4586b19c43a
Anish AP <72931920+anishp2199@users.noreply.github.com>
2023-04-14 12:04:33 -0700

    Add files via upload

---
+++ App_Client.py
@@ -0,0 +1,11 @@
+import socket
+import threading
+
+PORT = 1234
+SERVER = "localhost"
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 1234))
+
+msg = s.recv(1024)   #Decide how much data we want to receive at a time (buffer)
+print(msg.decode("utf-8"))   #sockets communicating in bytes
