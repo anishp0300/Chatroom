801c5a39701ef6c788f56e43a7c3da0d29cae899
Anish AP <72931920+anishp2199@users.noreply.github.com>
2023-04-20 12:49:07 -0700

    Update App_Server.py

--- App_Server.py
+++ App_Server.py
@@ -1,49 +1,26 @@
-#This is the code for server side for the chat 
-import socket
-import threading
 
-PORT = 1234
+import socket
+
+PORT=1234
 SERVER = "localhost"
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((SERVER, PORT))
-s.listen()   #incase of multiple connections, queue of 5
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((SERVER, PORT))
 
-def start_server():
-    print('Connection has been established on {}:{}'.format(SERVER, PORT))
+server.listen()
+client, addr = server.accept()
 
-    #listen for conenctions
-    while True:
-        clientsocket, address= s.accept()      #if anyone connects, accept
-        
-        print(f"Connection from {address} has been established!")
-        clientsocket.send("Welcome to the chat room".encode( "utf-8"))
-        
-        break
-        
-        
+print('Connection has been established on {}:{}'.format(SERVER, PORT))
 
-        #new thread creation for client connections
-        #c_thread = threading.Thread(target= handle_client, args=(c_socket, c_address))
-        #c_thread.start()
-        
-    while True:
-        inputBuffer = str(input())
-        clientsocket.send(inputBuffer.encode("utf-8"))
-        # try:
-        #     msg = clientsocket.recv(1024)
-        #     if msg: 
-        #         print(msg.decode("utf-8"))
-        
-        # except:
-        #     print("Error: Closing sockets.")
-        #     clientsocket.close()
-        #     break
-        
+done = False
+while not done:
 
-def broadcast(message):
-    clientsocket.send(message.encode("utf-8"))
+    msg = (client.recv(1024).decode('utf-8'))
+    if msg == 'quit':
+        done = True
+    else:
+        print(msg)
+        client.send(input("message: ").encode('utf-8'))
 
-
-#start the server
-start_server()
+client.close()
+server.close()
