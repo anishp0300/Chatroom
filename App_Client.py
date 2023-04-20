9371a1918b85b7794ac26bc103ebad3ce1a3f0a3
Anish AP <72931920+anishp2199@users.noreply.github.com>
2023-04-20 12:48:37 -0700

    Update App_Client.py
    Updated Client Code

--- App_Client.py
+++ App_Client.py
@@ -1,37 +1,32 @@
 import socket
-import threading
 
-#~~~~~~~~~~~#
-# FUNCTIONS #
-#~~~~~~~~~~~#
-def receive_messages():
-    # while True:
-        msg = serversocket.recv(1024).decode("utf-8")
-        if msg:
-            print(msg)
 
-def connect_to_server():
-	serversocket.connect(('localhost', 1234))
-	print("-Connection established-")
-	msg = serversocket.recv(1024)   #Decide how much data we want to receive at a time (buffer)
-	print(msg.decode("utf-8"))   #sockets communicating in bytes
-
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER= "localhost"
 PORT = 1234
-SERVER = "localhost"
 
-#~~~~~~~~~~~#
-# MAIN LOOP #
-#~~~~~~~~~~~#
-while True:
-	inputBuffer = str(input())
-	if(inputBuffer == "CONNECT"):
-		connect_to_server()
-	elif(inputBuffer == "QUIT"):
-		print("Closing program...")
-		break
-	elif(inputBuffer == "RECEIVE"):
-		receive_messages()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	# serversocket.send(inputBuffer.encode("utf-8"))
-	# receive_messages()
+client.connect((SERVER, PORT))
+
+done = False
+while not done:
+
+
+    print("-Connection Established-")
+    client.send(input("message: ").encode('utf-8'))
+    msg = (client.recv(1024).decode('utf-8'))
+    if msg == 'quit':
+        done = True
+    else:
+        print(msg)
+
+client.close()
+
+
+#~~~~~~~~~~~~~~~~~#
+   # MAIN LOOP #
+#~~~~~~~~~~~~~~~~~#
+
+
+
+
