a1c84cf5a2c70194bb8cd0a735b5ea709794152c
Shoh Sewell <shohsewell@gmail.com>
2023-04-21 16:55:28 -0700

    Cleaned up server<->client messaging. Added asynchronous receive messages.
    The Client will now receive messages from the server asynchronously on a separate thread and the server to client and vice versa communication has been cleaned up.

--- App_Client.py
+++ App_Client.py
@@ -1,11 +1,18 @@
+import threading
 import socket
 
 #-----------
 #FUNCTIONS
 #-----------
-def receive_messages():
-	msg = (client.recv(1024).decode('utf-8'))
-	print("Server: " + msg)
+def receive_messages(client):
+	while not done:
+		try:
+			msg = (client.recv(1024).decode('utf-8'))
+			if not done: 
+				print("\nServer: " + msg + "\n>>")
+		except:
+			print("Terminating receive thread")
+			break
 
 SERVER= "localhost"
 PORT = 1234
@@ -15,12 +22,17 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 done = False
 while not done:
 
+	# Make client receive messages on a separate thread
+	receive_thread = threading.Thread(target=receive_messages, args=(client,))
+
 	# Allows user to enter commands
 	inputBuffer = input(">> ")
 	if inputBuffer == "CONNECT":
 		try: # Try connecting to the server when CONNECT is entered
 			client.connect((SERVER, PORT))
 			print("-Connection Established-")
+			# Start receive thread once client has connected
+			receive_thread.start()
 		except: # Should the attempt fail, display an error so the program doesn't crash
 			print("ERROR: Connection Failed.")
 
@@ -28,12 +40,13 @@ while not done:
 	elif inputBuffer == "QUIT":
 		client.send(inputBuffer.encode('utf-8'))
 		done = True
+		#receive_thread.join(timeout=0.1)
 
 	# Checks for SEND <message> with SEND and <message> separated by a space (' ')
 	elif inputBuffer.split(' ')[0] == "SEND":
 		client.send(inputBuffer[5:].encode('utf-8'))
 		# After a message is sent, receive messages from the server
-		receive_messages()
+		# receive_messages()
 	
 	# If the wrong command is entered display an error message
 	else:
--- App_Server.py
+++ App_Server.py
@@ -4,7 +4,7 @@ import socket
 #-----------
 #FUNCTIONS
 #-----------
-def serving_client(client):
+def serving_client(client, addr):
     done = False
     while not done:
 
@@ -12,10 +12,10 @@ def serving_client(client):
         if msg == 'QUIT':
             done = True
             client.close()
-            print("Connection with {}:{} has been closed.".format(SERVER,PORT))
+            print("Connection with {} has been closed.".format(addr))
         else:
-            print(msg)
-            client.send(input("message: ").encode('utf-8'))
+            print("{}: {}".format(addr,msg))
+            # client.send(input("message: ").encode('utf-8'))
 
 def receive_messages(client):
 	return(client.recv(1024).decode('utf-8'))
@@ -27,19 +27,23 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((SERVER, PORT))
 
 server.listen()
-clients = []
 numClients = 0
+
+client_threads = []
 
 while True:
     client, addr = server.accept()
-
+    
     print('Connection has been established with {}'.format(addr))
 
-    client_thread = threading.Thread(target=serving_client, args=(client,))
+    client_threads.insert(0, threading.Thread(target=serving_client, args=(client,addr)))
 
-    client_thread.start()
+    client_threads[0].start()
 
     #client_thread.join()
+
+    # if input() == "QUIT":
+    #     break
 
 client.close()
 server.close()
